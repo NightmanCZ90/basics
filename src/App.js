8c8e631ab6026815068f29a8680b7dd2821cd902
NightmanCZ90 <novak.jan90@gmail.com>
2020-09-24 23:53:56 +0200

    useReducer basics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CounterHooks from './CounterHooks';
 import Toggler from './Toggler';
 import SimpleFormHooks from './SimpleFormHooks';
@@ -7,15 +7,27 @@ import Clicker from './Clicker';
 import SWMovies from './SWMovies';
 import './App.css';
 
+function countReducer(state, action) {
+  switch(action.type) {
+    case "INCREMENT":
+      return {count: state.count + action.amount};
+    case "DECREMENT":
+      return {count: state.count - action.amount};
+    case "RESET":
+      return {count: 0};
+  }
+  // if (action.type === "INCREMENT") return {count: state.count + action.amount}
+  // if (action.type === "DECREMENT") return {count: state.count - action.amount}
+}
 function App() {
+  const [state, dispatch] = useReducer(countReducer, {count: 0});
   return (
     <div className="App">
-      {/* <CounterHooks />
-      <Toggler />
-      <SimpleFormHooks />
-      <SimpleFormInputHook />
-      <Clicker /> */}
-      <SWMovies />
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({type: "INCREMENT", amount: 1})}>Add 1</button>
+      <button onClick={() => dispatch({type: "INCREMENT", amount: 5})}>Add 5</button>
+      <button onClick={() => dispatch({type: "DECREMENT", amount: 1})}>Subtract 1</button>
+      <button onClick={() => dispatch({type: "RESET"})}>RESET</button>
     </div>
   );
 }
